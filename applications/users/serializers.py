491d53f028e5520762dde57c2cd56fdb5caf8547
Soul-Ninja <almazbeksyrymbekov@gmail.com>
2023-08-15 15:45:26 +0600

    refactor code

--- applications/users/serializers.py
+++ applications/users/serializers.py
@@ -3,6 +3,7 @@ from rest_framework.validators import UniqueValidator
 from applications.users.constants import ROLE_CHOICES, GENDER_CHOICES, EDUCATION_CHOICES
 from applications.users.models import Skill
 from applications.users.services import UserService, ExecutorService, CustomerService
+from applications.users.utils import profile_with_user_exists
 
 executor_unique_validator = UniqueValidator(ExecutorService.fetch_all())
 customer_unique_validator = UniqueValidator(CustomerService.fetch_all())
@@ -20,7 +21,7 @@ class SignUpSerializer(serializers.Serializer):
 
 
 class ExecutorListSerializer(serializers.Serializer):
-    avatar = serializers.ImageField()
+    avatar = serializers.ImageField(required=False)
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField()
     last_name = serializers.CharField()
@@ -33,7 +34,7 @@ class ExecutorRetrieveSerializer(ExecutorListSerializer):
     profession = serializers.CharField()
     salary = serializers.DecimalField(max_digits=10, decimal_places=2)
     education_level = serializers.ChoiceField(EDUCATION_CHOICES)
-    location = serializers.CharField(validators=[executor_unique_validator])
+    location = serializers.CharField()
     phone = serializers.CharField(validators=[executor_unique_validator])
     contacts = serializers.StringRelatedField(many=True, read_only=True)
     educations = serializers.StringRelatedField(many=True, read_only=True)
@@ -43,6 +44,18 @@ class ExecutorRetrieveSerializer(ExecutorListSerializer):
 
 class ExecutorValidateSerializer(ExecutorRetrieveSerializer):
     skills = serializers.PrimaryKeyRelatedField(many=True, queryset=Skill.objects.all())
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=UserService.fetch_all(),
+        validators=[executor_unique_validator]
+    )
+
+    def create(self, validated_data):
+        executor = ExecutorService.create_profile(validated_data)
+        return executor
+
+    def update(self, instance, validated_data):
+        executor = ExecutorService.update_profile(instance, validated_data)
+        return executor
 
 
 class CustomerSerializer(ExecutorListSerializer):
@@ -51,4 +64,17 @@ class CustomerSerializer(ExecutorListSerializer):
 
 class CustomerValidateSerializer(ExecutorListSerializer):
     phone = serializers.CharField(validators=[customer_unique_validator])
-    location = serializers.CharField(validators=[customer_unique_validator])
+    location = serializers.CharField()
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=UserService.fetch_all(),
+        validators=[profile_with_user_exists]
+    )
+
+    def create(self, validated_data):
+        user = validated_data["user"]
+        customer = CustomerService.create_profile(validated_data)
+        return customer
+
+    def update(self, instance, validated_data):
+        customer = CustomerService.update_profile(instance, validated_data)
+        return customer
