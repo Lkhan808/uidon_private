008e0c8b5522954ed171f008532206c6d0b6eddc
Эльхан <pirigamelkhan@icloud.com>
2023-09-06 17:30:30 +0600

    дохуя че сделал

--- applications/users/urls.py
+++ applications/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import sign_up_view, verify_email_view, sign_in_view, password_reset, password_reset_confirm, change_password_view
+from .views import sign_up_view, verify_email_view, sign_in_view, password_reset, password_reset_confirm, change_password_view, change_email_view
 
 urlpatterns = [
     path('signup/', sign_up_view),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view()),
     path('password-reset/', password_reset, name='password-reset'),
     path('password-reset/confirm/<str:uid>/<str:token>/', password_reset_confirm, name='password-reset-update'),
-    path('change-password/', change_password_view, name='change-password')
+    path('change-password/', change_password_view, name='change-password'),
+    path('change-email/', change_email_view, name='change-email')
 ]
 
