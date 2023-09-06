7928e3fc46c64cca75394d47efafa67f9a126804
ARDAGER <abdyrakhmanov.arzybek@gmail.com>
2023-09-06 14:20:53 +0600

    изменил апишки заказов кастомера

--- applications/orders/urls.py
+++ applications/orders/urls.py
@@ -18,8 +18,6 @@ from .views import (
 urlpatterns = [
     path('create-order/', create_order, name='create-order'),
     path('orders/', list_orders, name='list-orders'),
-    path('customer/orders/', customer_orders_list, name='customer-orders'),
-    path('customer/close/orders/', customer_close_orders_list, name='customer-close-orders'),
     path('order/<int:order_id>/', order_detail, name='order-detail'),
     path('order/respond-to-order/', create_order_response, name='create-order-response'),
     path('order/<int:order_id>/responses/', list_responses_for_order, name='list-responses-for-order'),
@@ -28,5 +26,7 @@ urlpatterns = [
     path('executor/orders/<int:order_id>/close/', close_order_view, name='close-order'),
     path('order/add-favorite/<int:order_id>/', add_to_favorite, name='add-to-favorite'),
     path('order/favorite-list/executor/', favorite_orders_list, name='favorite-executors-list'),
-    path('order/delete/<int:order_id>/', remove_from_favorite, name='remove-executor-favorites')
+    path('order/delete/<int:order_id>/', remove_from_favorite, name='remove-executor-favorites'),
+    path('customer/orders/list', customer_orders_list, name='customer-orders-list'),
+    path('customer/close/orders/list', customer_close_orders_list, name='customer-close-orders-list'),
 ]
